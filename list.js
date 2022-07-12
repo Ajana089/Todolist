2e3ba1ffb727b1e12eece4a0776ed9cf73cbad42
ajanaantony089@gmail.com <ajanaantony089@gmail.com>
2022-07-12 14:00:42 +0400

    update

--- list.js
+++ list.js
@@ -1,48 +1,29 @@
      
-         var checkid=[];
+        
          var count=0;
-         var re="";
-         var test="";
-
          var response;
          var checkBox = document.createElement("input");
          var label = document.createElement("label");
-         checkBox.type = "checkbox";
-        function change(){
+          checkBox.type = "checkbox";
+           function change(){
                
              var xhttp=new XMLHttpRequest();
              xhttp.onreadystatechange=function(){
                 if(this.readyState==4&&this.status==200){
                    var response=JSON.parse(this.responseText);
-                     console.log(response);
-                   
                     
-               //       for(var i=0;i<response.length;i++)
-               //       {
-               //         re +="<li>"+ response[i].title + "</td><td></td><td>" + `<input type="checkbox" class="checkbox" value="checked" id=${checkid} ` + "</li>"
-               //          checkid=`${response[i].id}`; 
-               //       //re +="<li>"+ response[i].title + "</td><td></td><td>" ;
-               //       // var checkbox = document.createElement('input');
-               //       // checkbox.type = "checkbox";
-               //       // checkbox.name = "name" + response[i].title ;
-               //       // checkbox.value = "value";
-               //       // checkbox.id = "id" + i;
-               //       // re += + response[i].title ;
-              //    }
-  
-                 //  document.getElementById("demo").innerHTML=re;
-                 var myDiv = document.getElementById("cboxes");
+                   var myDiv = document.getElementById("cboxes");
                  for (var i = 0; i < response.length; i++) {
                   var checkBox = document.createElement("input");
                   var label = document.createElement("label");;
 
                   checkBox.type = "checkbox";
                   
-                  checkBox.value ="<li>"+ response[i].title +"</li>";
+                  checkBox.value = response[i].title + "\n";
                   myDiv.appendChild(checkBox) ;
                   myDiv.appendChild(label);
-                 label.appendChild( document.createTextNode(response[i].title));
-               
+                 label.appendChild( document.createTextNode(response[i].title))+" <br/> " ;
+                 
                 
                }
                  
@@ -54,23 +35,6 @@
             
 }
 
-// function checkboxes(){
-//    var inputElems = document.getElementsByTagName("input"),
-//    count = 0;
-//    for (var i=0; i<inputElems.length; i++) {
-//    if (inputElems[i].type === "checkbox" && inputElems[i].checked === true){
-//        count++;
-      
-//    }
-   
-//    if(count==5)
-//    {
-//       alert("Congrats. 5 Tasks have been Successfully Completed ” ")
-//       break;
-//    }
-// }
-
-// }
 
 
 const getPromise = function() {
@@ -96,9 +60,9 @@ const getPromise = function() {
    });
 };
 
-// promise is triggered to resolve here, when calling the function
+
 getPromise().then((count) => {
-   console.log("count"+count);
+   //console.log("count"+count);
 });
 
 
